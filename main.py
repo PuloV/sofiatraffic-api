f4b3dcc7151d2a3aaec32c576579d1b143ddc8c9
PuloV <yordanpulov@gmail.com>
2015-07-11 00:21:21 +0300

    small fixes for the server

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from flask import Flask
 import datetime
+import time
+import os.path
 app = Flask(__name__)
 
 
-@app.route('/tramway/<number>/')
-@app.route('/tramway/<number>/<date>')
-def tramway(number, date=datetime.date.today()):
-    return "Tramway №{} for the day {}".format(number, date), 404
+@app.route('/<transport>/<number>/')
+@app.route('/<transport>/<number>/<display_date>/')
+def transport(transport, number, display_date=datetime.date.today()):
 
+    if transport != "tramway" and transport != "bus" and transport != "trolley":
+        return "Transport {} not supported".format(transport), 400
 
-@app.route('/bus/<number>/')
-@app.route('/bus/<number>/<date>')
-def bus(number, date=datetime.date.today()):
-    return "Bus №{} for the day {}".format(number, date), 404
+    try:
+        dt = time.strptime(display_date, "%Y-%m-%d")
+        if display_date > datetime.date.today().strftime("%Y-%m-%d"):
+            display_date = datetime.date.today()
+    except Exception:
+        return "Date format must be Y-m-d".format(), 400
 
+    json_file = "{}/{}_{}.json".format(display_date, transport, number)
+    print(json_file)
+    if not os.path.isfile(json_file):
+        return "Information about {} {} {} Not Found".format(transport, number, display_date), 404
 
-@app.route('/trolley/<number>/')
-@app.route('/trolley/<number>/<date>')
-def trolley(number, date=datetime.date.today()):
-    return "Trolley №{} for the day {}".format(number, date), 404
+    return open(json_file, "r").read(), 200
 
 
 if __name__ == '__main__':
